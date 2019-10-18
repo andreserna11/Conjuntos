93e976b20bce246819d41145289049de4c4673ba
David Castro <dcastro@virtualbeams.com>
2019-10-18 12:35:28 -0500

    se agrega el metodo "esVacio"

--- src/conjuntos/Conjunto.java
+++ src/conjuntos/Conjunto.java
@@ -14,20 +14,9 @@ public class Conjunto {
 
     public static void main(String[] args) throws Exception {
         ConjuntoLista conjuntoLista = new ConjuntoLista();
-        ConjuntoLista subconjuntoLista = new ConjuntoLista();
-        ConjuntoLista noSubconjuntoLista = new ConjuntoLista();
-        for (int i = 1; i <= 10; i++) conjuntoLista.agregar(i);
-        for (int i = 3; i <= 8; i++) subconjuntoLista.agregar(i);
-        for (int i = 8; i <= 12; i++) noSubconjuntoLista.agregar(i);
-        conjuntoLista.agregar(2);
-        conjuntoLista.getLista().recorrerLista();
-        System.out.println("---");
-        subconjuntoLista.getLista().recorrerLista();
-        System.out.println("es subconjunto: " + conjuntoLista.subconjunto(subconjuntoLista));
-        noSubconjuntoLista.getLista().recorrerLista();
-        System.out.println("es subconjunto: " + conjuntoLista.subconjunto(noSubconjuntoLista));
-        conjuntoLista.pertenece(1);
-        conjuntoLista.pertenece(2);
-
+        ConjuntoLista conjuntoLista2 = new ConjuntoLista();
+        conjuntoLista.agregar(1);
+        System.out.println("Es vacío: " + conjuntoLista.esVacio());
+        System.out.println("Es vacío: " + conjuntoLista2.esVacio());
     }
 }
