a8cd7b46d3527d1472bcf0c176db69395ebbc03d
David Castro <dcastro@virtualbeams.com>
2019-10-18 12:28:44 -0500

    se agrega el metodo "subconjunto"

--- src/conjuntos/Conjunto.java
+++ src/conjuntos/Conjunto.java
@@ -14,9 +14,20 @@ public class Conjunto {
 
     public static void main(String[] args) throws Exception {
         ConjuntoLista conjuntoLista = new ConjuntoLista();
-        conjuntoLista.insertarNodo(2);
-        conjuntoLista.recorrerLista();
+        ConjuntoLista subconjuntoLista = new ConjuntoLista();
+        ConjuntoLista noSubconjuntoLista = new ConjuntoLista();
+        for (int i = 1; i <= 10; i++) conjuntoLista.agregar(i);
+        for (int i = 3; i <= 8; i++) subconjuntoLista.agregar(i);
+        for (int i = 8; i <= 12; i++) noSubconjuntoLista.agregar(i);
+        conjuntoLista.agregar(2);
+        conjuntoLista.getLista().recorrerLista();
+        System.out.println("---");
+        subconjuntoLista.getLista().recorrerLista();
+        System.out.println("es subconjunto: " + conjuntoLista.subconjunto(subconjuntoLista));
+        noSubconjuntoLista.getLista().recorrerLista();
+        System.out.println("es subconjunto: " + conjuntoLista.subconjunto(noSubconjuntoLista));
         conjuntoLista.pertenece(1);
         conjuntoLista.pertenece(2);
+
     }
 }
