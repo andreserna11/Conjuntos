a2970102f15709f32936c31e512bdd9757613425
David Castro <dcastro@virtualbeams.com>
2019-10-18 13:14:31 -0500

    se agrega el metodo "union"

--- src/conjuntos/Conjunto.java
+++ src/conjuntos/Conjunto.java
@@ -9,14 +9,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 import co.com.listas.entidades.ConjuntoLista;
+import co.com.vector.entidades.ConjuntoVector;
 
 public class Conjunto {
 
     public static void main(String[] args) throws Exception {
         ConjuntoLista conjuntoLista = new ConjuntoLista();
         ConjuntoLista conjuntoLista2 = new ConjuntoLista();
-        conjuntoLista.agregar(1);
-        System.out.println("Es vacío: " + conjuntoLista.esVacio());
-        System.out.println("Es vacío: " + conjuntoLista2.esVacio());
+        for (int i = 1; i <= 10; i++) conjuntoLista.agregar(i);
+        for (int i = 5; i <= 13; i++) conjuntoLista2.agregar(i);
+        conjuntoLista.mostrar();
+        conjuntoLista2.mostrar();
+        ConjuntoLista conjuntoLista3 = (ConjuntoLista) conjuntoLista.union(conjuntoLista2);
+        conjuntoLista3.mostrar();
     }
 }
