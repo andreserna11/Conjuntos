7817a3619ec684644b3663588ac111fa81b3da02
David Castro <dcastro@virtualbeams.com>
2019-10-18 13:35:17 -0500

    se agrega el metodo "igualdad"

--- src/conjuntos/Conjunto.java
+++ src/conjuntos/Conjunto.java
@@ -16,15 +16,13 @@ public class Conjunto {
     public static void main(String[] args) throws Exception {
         ConjuntoLista conjuntoLista = new ConjuntoLista();
         ConjuntoLista conjuntoLista2 = new ConjuntoLista();
+        ConjuntoLista conjuntoLista3 = new ConjuntoLista();
         for (int i = 1; i <= 10; i++) conjuntoLista.agregar(i);
-        for (int i = 13; i >= 5; i--) conjuntoLista2.agregar(i);
-        conjuntoLista.mostrar();
-        conjuntoLista2.mostrar();
-        ConjuntoLista conjuntoLista3 = (ConjuntoLista) conjuntoLista.union(conjuntoLista2);
-        ConjuntoLista conjuntoLista4 = (ConjuntoLista) conjuntoLista.interseccion(conjuntoLista2);
-        ConjuntoLista conjuntoLista5 = (ConjuntoLista) conjuntoLista2.interseccion(conjuntoLista);
-        conjuntoLista3.mostrar();
-        conjuntoLista4.mostrar();
-        conjuntoLista5.mostrar();
+        for (int i = 11; i <= 20; i++) conjuntoLista2.agregar(i);
+        for (int i = 1; i <= 10; i++) conjuntoLista3.agregar(i);
+        System.out.println("igualdad entre 1 y 2: " + conjuntoLista.igualdad(conjuntoLista2));
+        System.out.println("igualdad entre 2 y 3: " + conjuntoLista2.igualdad(conjuntoLista3));
+        System.out.println("igualdad entre 1 y 3: " + conjuntoLista.igualdad(conjuntoLista3));
+        
     }
 }
