0057c0490d0f4567c3a82b91d8320e0d53f3cd28
David Castro <dcastro@virtualbeams.com>
2019-10-18 13:55:20 -0500

    se agrega el metodo "borrar"

--- src/conjuntos/Conjunto.java
+++ src/conjuntos/Conjunto.java
@@ -15,14 +15,14 @@ public class Conjunto {
 
     public static void main(String[] args) throws Exception {
         ConjuntoLista conjuntoLista = new ConjuntoLista();
-        ConjuntoLista conjuntoLista2 = new ConjuntoLista();
-        ConjuntoLista conjuntoLista3 = new ConjuntoLista();
-        for (int i = 1; i <= 10; i++) conjuntoLista.agregar(i);
-        for (int i = 11; i <= 20; i++) conjuntoLista2.agregar(i);
-        for (int i = 1; i <= 10; i++) conjuntoLista3.agregar(i);
-        System.out.println("igualdad entre 1 y 2: " + conjuntoLista.igualdad(conjuntoLista2));
-        System.out.println("igualdad entre 2 y 3: " + conjuntoLista2.igualdad(conjuntoLista3));
-        System.out.println("igualdad entre 1 y 3: " + conjuntoLista.igualdad(conjuntoLista3));
+        for (int i = 1; i <= 5; i++) conjuntoLista.agregar(i);
+        conjuntoLista.mostrar();
+        conjuntoLista.borrar(3);
+        conjuntoLista.mostrar();
+        conjuntoLista.borrar(5);
+        conjuntoLista.mostrar();
+        conjuntoLista.borrar(1);
+        conjuntoLista.mostrar();
         
     }
 }
